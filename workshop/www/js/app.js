d0ba8243a9d28e44708d1accfa391f6d1f4abadf
s-morohara <g2116048@fun.ac.jp>
2016-08-23 02:55:39 +0900

    try using handlebars templates

--- workshop/www/js/app.js
+++ workshop/www/js/app.js
@@ -2,6 +2,9 @@
 (function () {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
+    var homeTpl = Handlebars.compile($("#home-tpl").html());
+    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
+
     var service = new EmployeeService();
     service.initialize().done(function () {
         renderHomeView();
@@ -16,22 +19,12 @@
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
         service.findByName($('.search-key').val()).done(function (employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.content').html(employeeListTpl(employees));
         });
     }
 
     function renderHomeView() {
-        var html =
-          "<h1>Directory</h1>" +
-          "<input class='search-key' type='search' placeholder='Enter name'/>" +
-          "<ul class='employee-list'></ul>";
-        $('body').html(html);
+        $('body').html(homeTpl());
         $('.search-key').on('keyup', findByName);
     }
 
